b04b8be87f4ac1ef21b786407d40bc706ffd4cfd
Raphaeln65 <167972997+Raphaeln65@users.noreply.github.com>
2025-08-20 10:49:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function enviarMensagemWhatsApp(event) {
+    event.preventDefault();
+    const nome = document.getElementById('nome').value.trim();
+    const mensagem = document.getElementById('mensagem').value.trim();
+
+    if (!nome || !mensagem) {
+        alert("Por favor, preencha todos os campos antes de enviar!");
+        return;
+    }
+
+    const texto = `Olá, meu nome é ${nome}. ${mensagem}`;
+    const numeroWhatsApp = '5528999455351';
+    const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
+
+    window.open(urlWhatsApp, '_blank');
+
+    document.getElementById('nome').value = '';
+    document.getElementById('mensagem').value = '';
+}
